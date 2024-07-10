619bceaef1a54b9522b814125616b68316248906
Fagner <fagnerfpr@gmail.com>
2024-07-10 03:32:48 -0300

    add more previews

--- app/views/components/button_component.rb
+++ app/views/components/button_component.rb
@@ -7,28 +7,37 @@ class ButtonComponent < ApplicationComponent
     lg: 'h-12 px-6 text-sm'
   }
 
-  def initialize(size: :md)
+  def initialize(size: :md, elevated: false, badge: nil, disabled: false)
     @size = size
+    @elevated = elevated
+    @badge = badge
+    @disabled = disabled
   end
 
   def view_template
     button(
-      class: [
-        'inline-flex items-center justify-center gap-2',
-        'font-medium tracking-wide text-white',
-        'transition duration-300 rounded focus-visible:outline-none whitespace-nowrap',
-        'bg-emerald-500 hover:bg-emerald-600 focus:bg-emerald-700',
-        'disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none',
-        SIZES[@size]
-      ]
+      class: button_classes, disabled: @disabled
     ) do
-      span do
-        if block_given?
-          yield
-        else
-          'Text example'
-        end
+      if block_given?
+        yield
+      else
+        span { 'Text example' }
       end
     end
+  end
+
+  private
+
+  def button_classes
+    classes = [
+      'inline-flex items-center justify-center gap-2',
+      'font-medium tracking-wide text-white',
+      'transition duration-300 rounded focus-visible:outline-none whitespace-nowrap',
+      'bg-emerald-500 hover:bg-emerald-600 focus:bg-emerald-700',
+      'disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none',
+      SIZES[@size]
+    ]
+    classes << 'shadow-md shadow-emerald-200 hover:shadow-sm hover:shadow-emerald-200 focus:shadow-sm focus:shadow-emerald-200' if @elevated
+    classes.join(' ')
   end
 end
--- test/components/previews/button_component_preview.rb
+++ test/components/previews/button_component_preview.rb
@@ -1,10 +1,25 @@
 # frozen_string_literal: true
 
 class ButtonComponentPreview < Lookbook::Preview
+  # @!group Types
+
   # @param size [Symbol] select { choices: [ ":sm", ":md", ":lg" ] }
   def default(size: :md)
-    render ButtonComponent.new(size: size) do
+    render ButtonComponent.new(size:) do
       'Text example'
     end
   end
+
+  def elevated
+    render ButtonComponent.new(elevated: true) do
+      'Text example'
+    end
+  end
+
+  def disabled
+    render ButtonComponent.new(disabled: true) do
+      'Text example'
+    end
+  end
+  # @!endgroup
 end
